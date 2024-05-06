c5717df9855d4b0c06d92c41e767b6eec5d55c13
WesRamox <wesleyramox258@gmail.com>
2024-05-06 01:42:37 -0300

    Update create profile with modal

--- public/script/app.js
+++ public/script/app.js
@@ -18,22 +18,41 @@ setInterval(
 	}, 10000
 )
 
+
 function newProfile() {
     const ulProfiles = document.getElementById("profile-list")
     const itemsLi = document.getElementsByClassName("boxUser")
-
+    const inputName = document.getElementById("user-name")
+    
     if(itemsLi.length < 4) {
         let newUser = document.createElement("div")
         newUser.className = "boxUser"
-
+        
         newUser.innerHTML = `
         <li>
-            <img src="./public/images/profileDefault.png" alt="Profile Image" id="profileImg">
+        <img src="./public/images/profileDefault.png" alt="Profile Image" id="profileImg">
         </li>
-        <p id="profile-name">Wesley</p>
+        <p id="profile-name">${inputName.value}</p>
         `
-
-        ulProfiles.appendChild(newUser)           
+        
+        ulProfiles.appendChild(newUser)  
+        inputName.value = ""         
     }
+    modalNewUser()
 }
 
+function modalNewUser() {
+    const sectionModal = document.getElementById("createProfile")
+    const container = document.querySelector(".container")
+    
+    if(container.classList.contains("hidden")) {
+        container.classList.remove("hidden")
+        sectionModal.classList.add("hidden")
+    } else {
+        sectionModal.classList.remove("hidden")
+        container.classList.add("hidden")
+    }
+    
+}
+
+loadData()
